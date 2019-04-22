7b61b9cb35b728fe630c3919d005d9cabd21dbd9
huajieyu <libojiang8584@gmail.com>
2019-04-22 14:31:48 -0500

    updated the script : maker_det_cov_matrix.py

--- Sandbox/DetectorSyst/make_det_cov_matrix.py
+++ Sandbox/DetectorSyst/make_det_cov_matrix.py
@@ -6,19 +6,27 @@ gROOT.ProcessLine(".x " + os.environ['MYSW_DIR'] + "/Utils/rootlogon.C")
 
 from array import array
 
+print "start to open the root file and get the histograms"
+
 # Get the central value xsec
 file_cv = TFile("xsec_file_CV.root");
 xsec_onebin_cv = file_cv.Get("xsec_onebin_CV")
 xsec_mumom_cv = file_cv.Get("xsec_mumom_CV")
 xsec_muangle_cv = file_cv.Get("xsec_muangle_CV")
-xsec_muangle_mumom_cv = file_cv.Get("xsec_muangle_mumom_CV")
-xsec_poly_muangle_mumom_cv = file_cv.Get("xsec_poly_muangle_mumom_CV")
+xsec_pmom_cv = file_cv.Get("xsec_pmom_CV")
+xsec_pangle_cv = file_cv.Get("xsec_pangle_CV")
 
 print "Number of muon momentum bins", xsec_mumom_cv.GetNbinsX()
 print "Number of muon angle bins", xsec_muangle_cv.GetNbinsX()
+print "Number of proton momentum bins", xsec_pmom_cv.GetNbinsX()
+print "Number of proton momentum bins", xsec_pangle_cv.GetNbinsX()
 
 n_bins_mumom = xsec_mumom_cv.GetNbinsX()
 n_bins_muangle = xsec_muangle_cv.GetNbinsX()
+n_bins_pmom = xsec_pmom_cv.GetNbinsX()
+n_bins_pangle = xsec_pangle_cv.GetNbinsX()
+
+print "start to build histograms for the covariance matrix and fractional covariance matrix"
 
 # The cov matrix
 cov_matrix_mumom = TH2D("cov_matrix_mumom", "", n_bins_mumom, 0, n_bins_mumom, n_bins_mumom, 0, n_bins_mumom)
@@ -26,16 +34,12 @@ cov_matrix_mumom_frac = TH2D("cov_matrix_mumom_frac", "", n_bins_mumom, 0, n_bin
 cov_matrix_muangle = TH2D("cov_matrix_muangle", "", n_bins_muangle, 0, n_bins_muangle, n_bins_muangle, 0, n_bins_muangle)
 cov_matrix_muangle_frac = TH2D("cov_matrix_muangle_frac", "", n_bins_muangle, 0, n_bins_muangle, n_bins_muangle, 0, n_bins_muangle)
 
-n_bins_mumom = xsec_muangle_mumom_cv.GetNbinsY()
-n_bins_muangle = xsec_muangle_mumom_cv.GetNbinsX()
+cov_matrix_pmom = TH2D("cov_matrix_pmom", "", n_bins_pmom, 0, n_bins_pmom, n_bins_pmom, 0, n_bins_pmom)
+cov_matrix_pmom_frac = TH2D("cov_matrix_pmom_frac", "", n_bins_pmom, 0, n_bins_pmom, n_bins_pmom, 0, n_bins_pmom)
+cov_matrix_pangle = TH2D("cov_matrix_pangle", "", n_bins_pangle, 0, n_bins_pangle, n_bins_pangle, 0, n_bins_pangle)
+cov_matrix_pangle_frac = TH2D("cov_matrix_pangle_frac", "", n_bins_pangle, 0, n_bins_pangle, n_bins_pangle, 0, n_bins_pangle)
 
-cov_matrix_muangle_mumom = TH2D("cov_matrix_muangle_mumom", "", n_bins_mumom*n_bins_muangle, 0, n_bins_mumom*n_bins_muangle, n_bins_mumom*n_bins_muangle, 0, n_bins_mumom*n_bins_muangle)
-cov_matrix_muangle_mumom_frac = TH2D("cov_matrix_muangle_mumom_frac", "", n_bins_mumom*n_bins_muangle, 0, n_bins_mumom*n_bins_muangle, n_bins_mumom*n_bins_muangle, 0, n_bins_mumom*n_bins_muangle)
 
-n_bins_poly = xsec_poly_muangle_mumom_cv.GetNumberOfBins()
-print "Number of poly bins:", n_bins_poly
-cov_matrix_poly_muangle_mumom = TH2D("cov_matrix_poly_muangle_mumom", "", n_bins_poly, 0, n_bins_poly, n_bins_poly, 0, n_bins_poly)
-cov_matrix_poly_muangle_mumom_frac = TH2D("cov_matrix_poly_muangle_mumom_frac", "", n_bins_poly, 0, n_bins_poly, n_bins_poly, 0, n_bins_poly)
 
 
 print "CV cross section", xsec_onebin_cv.GetBinContent(1)
@@ -44,7 +48,7 @@ print "Systematic parameter & Cross section & Total xsec perc difference \\\\"
 
 print "CV & ", xsec_onebin_cv.GetBinContent(1), " &  0.0  \\\\"
 
-
+print "Start to reset the matrices to zero"
 
 #
 # Reset matrices to zero
@@ -60,43 +64,53 @@ for i in xrange(0, xsec_muangle_cv.GetNbinsX()):
 		cov_matrix_muangle.SetBinContent(i+1, j+1, 0) 
 		cov_matrix_muangle_frac.SetBinContent(i+1, j+1, 0)
 
-for i in xrange(0, n_bins_muangle):
-	for j in xrange(0, n_bins_mumom):
-		for m in xrange(0, n_bins_muangle):
-			for n in xrange(0, n_bins_mumom):
+for i in xrange(0, xsec_pmom_cv.GetNbinsX()):
+	for j in xrange(0, xsec_pmom_cv.GetNbinsX()):
+		cov_matrix_pmom.SetBinContent(i+1, j+1, 0) 
+		cov_matrix_pmom_frac.SetBinContent(i+1, j+1, 0)
 
-				a = j + i * n_bins_mumom + 1;
-				b = n + m * n_bins_mumom + 1;
-
-				cov_matrix_muangle_mumom.SetBinContent(a, b, 0)
-				cov_matrix_muangle_mumom_frac.SetBinContent(a, b, 0)
-
-for i in xrange(0, n_bins_poly):
-	for j in xrange(0, n_bins_poly):
-		cov_matrix_poly_muangle_mumom.SetBinContent(i+1, j+1, 0) 
-		cov_matrix_poly_muangle_mumom_frac.SetBinContent(i+1, j+1, 0)
-
-# for i in xrange(0, n_bins_muangle):
-# 	for j in xrange(0, n_bins_mumom):
-# 		print "Cross Section at ", i, j , xsec_muangle_mumom_cv.GetBinContent(i+1, j+1)
+for i in xrange(0, xsec_pangle_cv.GetNbinsX()):
+	for j in xrange(0, xsec_pangle_cv.GetNbinsX()):
+		cov_matrix_pangle.SetBinContent(i+1, j+1, 0) 
+		cov_matrix_pangle_frac.SetBinContent(i+1, j+1, 0)
 
 
 
 
 
+print "start to set the detector systematic list and statistic bin values for analysis"
 
-det_syst_list = ["CV", "dataSCE", "withDIC", "stretchResp", "squeezeResp", "DLdown", "DLup", "DTdown", "DTup", "LArG4BugFix", "downPEnoise", "upPEnoise", "noiseAmpDown", "noiseAmpUp"]
+#det_syst_list = ["CV", "DLdown", "DLup", "DTup", "DTdown", "noiseAmpUp", "noiseAmpDown", "squeezeResp", "stretchResp", "downPEnoise", "upPEnoise","LArG4BugFix", "altDeadChannels", "dataSCE", "deadSaturatedChannels", "enhancedexttpcvis","lifetime10ms", "withDIC"]
+#det_syst_list = ["CV", "DLdown", "DLup", "DTup", "DTdown", "noiseAmpUp", "noiseAmpDown", "squeezeResp", "stretchResp", "downPEnoise", "upPEnoise", "LArG4BugFix", "altDeadChannels", "lifetime10ms"]
+#det_syst_list = ["CV", "DLdown", "DLup", "DTup", "DTdown", "noiseAmpUp", "noiseAmpDown", "squeezeResp", "stretchResp", "downPEnoise", "upPEnoise", "LArG4BugFix", "altDeadChannels", "dataSCE", "enhancedexttpcvis","lifetime10ms", "withDIC"]
+#det_syst_list = ["CV", "DLdown"]
+#det_syst_list = ["CV", "DLup"]
+#det_syst_list = ["CV", "DTup"]
+#det_syst_list = ["CV", "DTdown"]
+det_syst_list = ["CV", "noiseAmpUp"]
+#det_syst_list = ["CV", "noiseAmpDown"]
+#det_syst_list = ["CV", "squeezeResp"]
+#det_syst_list = ["CV", "stretchResp"]
+#det_syst_list = ["CV", "downPEnoise"]
+#det_syst_list = ["CV", "upPEnoise"]
+#det_syst_list = ["CV", "LArG4BugFix"]
+#det_syst_list = ["CV", "altDeadChannels"]
+#det_syst_list = ["CV", "dataSCE"]
+#det_syst_list = ["CV", "enhancedexttpcvis"]
+#det_syst_list = ["CV", "lifetime10ms"]
+#det_syst_list = ["CV", "withDIC"]
 
-det_syst_list = ["CV", "dataSCE", "withDIC", "squeezeResp", "DLdown", "DTup", "LArG4BugFix", "downPEnoise", "noiseAmpUp"]
 
-det_syst_list = ["CV", "dataSCE", "withDIC"]
-
-stat_err_perbin_mumom = [0.0041, 0.013, 0.010, 0.0048, 0.0028, 0.00067]
-stat_err_perbin_muangle = [0.0034, 0.0026, 0.0060, 0.0038, 0.0055, 0.0087, 0.0080, 0.011, 0.018]
-stat_err_perbin_onebin = 0.0091
+#stat_err_perbin_mumom = [0.0041, 0.013, 0.010, 0.0048, 0.0028, 0.00067]
+#stat_err_perbin_muangle = [0.0034, 0.0026, 0.0060, 0.0038, 0.0055, 0.0087, 0.0080, 0.011, 0.018]
+#stat_err_perbin_pmom = []
+#stat_err_perbin_pangle = []
+#stat_err_perbin_onebin = 0.0091
 
 stat_err_perbin_mumom = [0, 0, 0, 0, 0, 0]
 stat_err_perbin_muangle = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+stat_err_perbin_pmom = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+stat_err_perbin_pangle = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 stat_err_perbin_onebin = 0
 
 syst_total_xsec = 0
@@ -104,32 +118,27 @@ syst_mumom = [0, 0, 0, 0, 0, 0]
 
 for syst_name in det_syst_list:
 
-	# if syst_name == "nodeltaray" or syst_name == "nospacecharge" or syst_name == "stretchRes" or syst_name == "stretchResp" or syst_name == "noShortedResp":
-	# 	continue
 
 	file_name = "xsec_file_" + syst_name + ".root"
 	file = TFile(file_name);
-	# if (file.IsOpen()):
-	# 	print "File with name", file_name, "is opened"
-	# else:
-	# 	print "Cannot find file", file_name
-	# 	exit(0)
+        if (file.IsOpen()):
+	 	print "File with name", file_name, "is opened"
+        else:
+	 	print "Cannot find file", file_name
+	 	exit(0)
 
  	xsec_onebin             = file.Get("xsec_onebin_" + syst_name)
  	xsec_mumom              = file.Get("xsec_mumom_" + syst_name)
  	xsec_muangle            = file.Get("xsec_muangle_" + syst_name)
- 	xsec_muangle_mumom      = file.Get("xsec_muangle_mumom_" + syst_name)
- 	xsec_poly_muangle_mumom = file.Get("xsec_poly_muangle_mumom_" + syst_name)
-
-	# for i in xrange(0, n_bins_muangle):
-	# 	for j in xrange(0, n_bins_mumom):
-	# 		print syst_name, "Cross Section at ", i, j , xsec_muangle_mumom.GetBinContent(i+1, j+1), xsec_muangle_mumom_cv.GetBinContent(i+1, j+1)
+ 	xsec_pmom               = file.Get("xsec_pmom_" + syst_name)
+        xsec_pangle             = file.Get("xsec_pangle_" + syst_name)
+        print file_name 
 
 
 	perc_diff = (xsec_onebin_cv.GetBinContent(1) - xsec_onebin.GetBinContent(1)) / xsec_onebin_cv.GetBinContent(1)
 	print syst_name, " & ", xsec_onebin.GetBinContent(1), " & ", perc_diff*100, "  \\\\"
 	syst_total_xsec = syst_total_xsec + abs(xsec_onebin_cv.GetBinContent(1) - xsec_onebin.GetBinContent(1))**2
-	#print "diff is ", abs(xsec_onebin_cv.GetBinContent(1) - xsec_onebin.GetBinContent(1))
+	print "diff is ", abs(xsec_onebin_cv.GetBinContent(1) - xsec_onebin.GetBinContent(1))
 
 	
 
@@ -145,11 +154,6 @@ for syst_name in det_syst_list:
 			cov_matrix_mumom_frac.SetBinContent(i+1, j+1, cov_matrix_mumom_frac.GetBinContent(i+1, j+1) + d0_frac)
 			# syst_mumom[i] = syst_mumom[i] + d0_frac
 
-		# if i == 0:s
-			# print "syst_mumom[0]", syst_mumom[0]
-			# print syst_name, "d0", d0, "d0_frac", d0_frac, "sqrt_d0_frac", math.sqrt(d0_frac), "xsec", xsec_mumom.GetBinContent(i+1), "nominal_xsec", xsec_mumom_cv.GetBinContent(i+1)
-		# print "systematic", syst_name, "d0", d0, "---", cov_matrix_mumom.GetBinContent(i+1, i+1)
-		# print "systematic", syst_name, "nominal", xsec_mumom_cv.GetBinContent(i+1), "variation", xsec_mumom.GetBinContent(i+1) 
 
   
 	for i in xrange(0, xsec_muangle.GetNbinsX()):
@@ -160,44 +164,29 @@ for syst_name in det_syst_list:
 			cov_matrix_muangle_frac.SetBinContent(i+1, j+1, cov_matrix_muangle_frac.GetBinContent(i+1, j+1) + d0_frac)
 
 
-	for i in xrange(0, n_bins_muangle):
-		for j in xrange(0, n_bins_mumom):
-			for m in xrange(0, n_bins_muangle):
-				for n in xrange(0, n_bins_mumom):
-					a = j + i * n_bins_mumom + 1;
-					b = n + m * n_bins_mumom + 1;
-					# print 'a', a, 'b', b
-					my_a = 12 #5
-					my_b = 12 #5
-					if (a==my_a and b==my_b): print 'Here starts', syst_name
-					if (a==my_a and b==my_b): print 'i', i, ', j', j, ', m', m, ', n', n
-					if (a==my_a and b==my_b): print 'syst', xsec_muangle_mumom.GetBinContent(i+1, j+1), '   cv', xsec_muangle_mumom_cv.GetBinContent(i+1, j+1)
-					d0 = (xsec_muangle_mumom.GetBinContent(i+1, j+1) - xsec_muangle_mumom_cv.GetBinContent(i+1, j+1))*(xsec_muangle_mumom.GetBinContent(m+1, n+1) - xsec_muangle_mumom_cv.GetBinContent(m+1, n+1))
-					den = ((xsec_muangle_mumom_cv.GetBinContent(i+1, j+1))*xsec_muangle_mumom_cv.GetBinContent(m+1, n+1))
-					cov_matrix_muangle_mumom.SetBinContent(a, b, cov_matrix_muangle_mumom.GetBinContent(a, b) + d0)
-					# print 'd0', d0
-					if (a==my_a and b==my_b): print 'd0', d0, 'den', den
-					if (den != 0):
-						d0_frac = d0 / den
-						if (a==my_a and b==my_b): print 'd0_frac', d0_frac 
-						cov_matrix_muangle_mumom_frac.SetBinContent(a, b, cov_matrix_muangle_mumom_frac.GetBinContent(a, b) + d0_frac)
-					if (a==my_a and b==my_b): print 'Here ends', syst_name
+	for i in xrange(0, xsec_pmom.GetNbinsX()):
+		for j in xrange(0, xsec_pmom.GetNbinsX()):
+			d0 = (xsec_pmom.GetBinContent(i+1) - xsec_pmom_cv.GetBinContent(i+1))*(xsec_pmom.GetBinContent(j+1) - xsec_pmom_cv.GetBinContent(j+1))
+			d0_frac = d0 / ((xsec_pmom_cv.GetBinContent(i+1))*(xsec_pmom_cv.GetBinContent(j+1)))
+			cov_matrix_pmom.SetBinContent(i+1, j+1, cov_matrix_pmom.GetBinContent(i+1, j+1) + d0)
+			cov_matrix_pmom_frac.SetBinContent(i+1, j+1, cov_matrix_pmom_frac.GetBinContent(i+1, j+1) + d0_frac)
+			# syst_pmom[i] = syst_pmom[i] + d0_frac
+
+
+  
+	for i in xrange(0, xsec_pangle.GetNbinsX()):
+		for j in xrange(0, xsec_pangle.GetNbinsX()):
+			d0 = (xsec_pangle.GetBinContent(i+1) - xsec_pangle_cv.GetBinContent(i+1))*(xsec_pangle.GetBinContent(j+1) - xsec_pangle_cv.GetBinContent(j+1))
+			d0_frac = d0 / ((xsec_pangle_cv.GetBinContent(i+1))*xsec_pangle_cv.GetBinContent(j+1))
+			cov_matrix_pangle.SetBinContent(i+1, j+1, cov_matrix_pangle.GetBinContent(i+1, j+1) + d0)
+			cov_matrix_pangle_frac.SetBinContent(i+1, j+1, cov_matrix_pangle_frac.GetBinContent(i+1, j+1) + d0_frac)
 
 
 
 
-	for i in xrange(0, n_bins_poly):
-		for j in xrange(0, n_bins_poly):
-			d0 = (xsec_poly_muangle_mumom.GetBinContent(i+1) - xsec_poly_muangle_mumom_cv.GetBinContent(i+1))*(xsec_poly_muangle_mumom.GetBinContent(j+1) - xsec_poly_muangle_mumom_cv.GetBinContent(j+1))
-			d0_frac = d0 / (xsec_poly_muangle_mumom_cv.GetBinContent(i+1)*xsec_poly_muangle_mumom_cv.GetBinContent(j+1))
-			cov_matrix_poly_muangle_mumom.SetBinContent(i+1, j+1, cov_matrix_poly_muangle_mumom.GetBinContent(i+1, j+1) + d0) 
-			cov_matrix_poly_muangle_mumom_frac.SetBinContent(i+1, j+1, cov_matrix_poly_muangle_mumom_frac.GetBinContent(i+1, j+1) + d0_frac)
 
-
-
-
-print "Total onebin syst err:", math.sqrt(syst_total_xsec)
-print "Total onebin syst err (relative):", math.sqrt(syst_total_xsec) / xsec_onebin_cv.GetBinContent(1)
+#print "Total onebin syst err:", math.sqrt(syst_total_xsec)
+#print "Total onebin syst err (relative):", math.sqrt(syst_total_xsec) / xsec_onebin_cv.GetBinContent(1)
 
 # for i in xrange(0, xsec_mumom_cv.GetNbinsX()):
 # 	print "bin", i+1, "sqrt of d0_frac", math.sqrt(syst_mumom[i]) 
@@ -284,12 +273,12 @@ cov_file = TFile("covariance_detector.root", "RECREATE");
 cov_file.cd();
 cov_matrix_mumom.Write("covariance_matrix_detector_mumom");
 cov_matrix_muangle.Write("covariance_matrix_detector_muangle");
-cov_matrix_muangle_mumom.Write("covariance_matrix_detector_muangle_mumom");
-cov_matrix_poly_muangle_mumom.Write("covariance_matrix_poly_detector_muangle_mumom");
+cov_matrix_pmom.Write("covariance_matrix_detector_pmom");
+cov_matrix_pangle.Write("covariance_matrix_detector_pangle");
 cov_matrix_mumom_frac.Write("frac_covariance_matrix_detector_mumom");
 cov_matrix_muangle_frac.Write("frac_covariance_matrix_detector_muangle");
-cov_matrix_muangle_mumom_frac.Write("frac_covariance_matrix_detector_muangle_mumom");
-cov_matrix_poly_muangle_mumom_frac.Write("frac_covariance_matrix_poly_detector_muangle_mumom");
+cov_matrix_pmom_frac.Write("frac_covariance_matrix_detector_pmom");
+cov_matrix_pangle_frac.Write("frac_covariance_matrix_detector_pangle");
 cov_file.Close();
 
 gStyle.SetPaintTextFormat("4.3f");
@@ -312,27 +301,51 @@ cov_matrix_muangle_frac.GetXaxis().CenterTitle();
 cov_matrix_muangle_frac.GetYaxis().CenterTitle();
 cov_matrix_muangle_frac.Draw("colz TEXT")
 
-gStyle.SetPaintTextFormat("4.2f");
-c_muangle_mumom = TCanvas()
-# cov_matrix_muangle_mumom_frac.SetMarkerColor(kWhite);
-cov_matrix_muangle_mumom_frac.SetMarkerSize(1.1);
-cov_matrix_muangle_mumom_frac.GetXaxis().SetTitle("Bin ij");
-cov_matrix_muangle_mumom_frac.GetYaxis().SetTitle("Bin mn");
-cov_matrix_muangle_mumom_frac.GetXaxis().CenterTitle();
-cov_matrix_muangle_mumom_frac.GetYaxis().CenterTitle();
-cov_matrix_muangle_mumom_frac.Draw("colz TEXT")
+gStyle.SetPaintTextFormat("4.3f");
+c_pmom = TCanvas()
+# cov_matrix_pmom_frac.SetMarkerColor(kWhite);
+cov_matrix_pmom_frac.SetMarkerSize(1.6);
+cov_matrix_pmom_frac.GetXaxis().SetTitle("Bin i");
+cov_matrix_pmom_frac.GetYaxis().SetTitle("Bin j");
+cov_matrix_pmom_frac.GetXaxis().CenterTitle();
+cov_matrix_pmom_frac.GetYaxis().CenterTitle();
+cov_matrix_pmom_frac.Draw("colz TEXT")
 
-gStyle.SetPaintTextFormat("4.2f");
-c_muangle_mumom_poly = TCanvas()
+
+gStyle.SetPaintTextFormat("4.3f");
+c_pangle = TCanvas()
+# cov_matrix_pangle_frac.SetMarkerColor(kWhite);
+cov_matrix_pangle_frac.SetMarkerSize(1.6);
+cov_matrix_pangle_frac.GetXaxis().SetTitle("Bin i");
+cov_matrix_pangle_frac.GetYaxis().SetTitle("Bin j");
+cov_matrix_pangle_frac.GetXaxis().CenterTitle();
+cov_matrix_pangle_frac.GetYaxis().CenterTitle();
+cov_matrix_pangle_frac.Draw("colz TEXT")
+
+
+
+
+#gStyle.SetPaintTextFormat("4.2f");
+#c_muangle_mumom = TCanvas()
 # cov_matrix_muangle_mumom_frac.SetMarkerColor(kWhite);
-cov_matrix_poly_muangle_mumom_frac.SetMarkerSize(1.1);
-cov_matrix_poly_muangle_mumom_frac.GetXaxis().SetTitle("Bin i");
-cov_matrix_poly_muangle_mumom_frac.GetYaxis().SetTitle("Bin j");
-cov_matrix_poly_muangle_mumom_frac.GetXaxis().CenterTitle();
-cov_matrix_poly_muangle_mumom_frac.GetYaxis().CenterTitle();
-cov_matrix_poly_muangle_mumom_frac.SetMaximum(1.5)
-cov_matrix_poly_muangle_mumom_frac.SetMinimum(-1.5)
-cov_matrix_poly_muangle_mumom_frac.Draw("colz TEXT")
+#cov_matrix_muangle_mumom_frac.SetMarkerSize(1.1);
+#cov_matrix_muangle_mumom_frac.GetXaxis().SetTitle("Bin ij");
+#cov_matrix_muangle_mumom_frac.GetYaxis().SetTitle("Bin mn");
+#cov_matrix_muangle_mumom_frac.GetXaxis().CenterTitle();
+#cov_matrix_muangle_mumom_frac.GetYaxis().CenterTitle();
+#cov_matrix_muangle_mumom_frac.Draw("colz TEXT")
+
+#gStyle.SetPaintTextFormat("4.2f");
+#c_muangle_mumom_poly = TCanvas()
+# cov_matrix_muangle_mumom_frac.SetMarkerColor(kWhite);
+#cov_matrix_poly_muangle_mumom_frac.SetMarkerSize(1.1);
+#cov_matrix_poly_muangle_mumom_frac.GetXaxis().SetTitle("Bin i");
+#cov_matrix_poly_muangle_mumom_frac.GetYaxis().SetTitle("Bin j");
+#cov_matrix_poly_muangle_mumom_frac.GetXaxis().CenterTitle();
+#cov_matrix_poly_muangle_mumom_frac.GetYaxis().CenterTitle();
+#cov_matrix_poly_muangle_mumom_frac.SetMaximum(1.5)
+#cov_matrix_poly_muangle_mumom_frac.SetMinimum(-1.5)
+#cov_matrix_poly_muangle_mumom_frac.Draw("colz TEXT")
 
 # gStyle.SetPaintTextFormat("4.3f");
 # c_mumom = TCanvas()
